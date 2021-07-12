10d851cf22a8c9017db8ca30a19c1a91d7a41708
ibukun-koyer <ji81778@umbc.edu>
2021-07-12 00:02:00 -0400

    attempting to commit videos

--- minority-app/src/context/courses.js
+++ minority-app/src/context/courses.js
@@ -15,14 +15,16 @@ function CoursesProvider({ children }) {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tempus curabitur id consectetur fringilla. Sed eget hendrerit convallis erat. Sit diam, tincidunt ultricies et mauris morbi. Morbi dolor semper proin eu aenean nunc egestas quam id. Feugiat elit sed erat purus faucibus risus. Elementum velit lorem ullamcorper ultricies velit, massa eu eget. Eget id libero tempus faucibus arcu ullamcorper purus tellus. Vulputate urna cras consectetur cras tellus imperdiet massa lacus at. Facilisis mollis eleifend aenean eget consectetur nisl arcu. Aenean fringilla velit malesuada et non pharetra tristique. Arcu consequat vitae nulla lacus, mattis. Ante diam in dapibus arcu, mauris arcu. Non ultricies nec volutpat dictum. Viverra gravida in egestas congue at vestibulum quis in. Nibh suspendisse mauris volutpat est ultrices mi. Lectus cursus euismod id dictum et eget pharetra in. Purus ullamcorper laoreet mauris pretium lacus ridiculus. Pulvinar vitae elit dignissim vel. Et commodo amet mi, sed vitae enim etiam rutrum bibendum.";
   const randomVideos = [
     {
-      videoLink: process.env.PUBLIC_URL + "/edited-covid-video.mp4",
-      videoTitle: "Creating a covid vis project",
+      videoLink:
+        process.env.PUBLIC_URL +
+        "/GitHub - Create Local Repository and push to github Push an existing repository to github.mp4",
+      videoTitle: "Creating git repositories",
       header,
       header2,
     },
     {
-      videoLink: process.env.PUBLIC_URL + "/Edited-project-notebook.mp4",
-      videoTitle: "Creating a notebook app",
+      videoLink: process.env.PUBLIC_URL + "/How to Create Twitter Account.mp4",
+      videoTitle: "Creating a twitter account",
       header,
       header2,
     },
